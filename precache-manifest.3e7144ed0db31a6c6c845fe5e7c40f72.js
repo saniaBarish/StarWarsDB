17a1778cf3e7fb302328c1dfd835d911ddca4c08
Barishpolets Oleksandr <barishpolets.oleksandr@pdffiller.team>
2018-12-10 17:00:19 +0200

    Updates

---
+++ precache-manifest.3e7144ed0db31a6c6c845fe5e7c40f72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9d155c1563a8a449bea",
+    "url": "/StarWarsDB/static/js/runtime~main.f9d155c1.js"
+  },
+  {
+    "revision": "c348d97c41185b2728d0",
+    "url": "/StarWarsDB/static/js/main.c348d97c.chunk.js"
+  },
+  {
+    "revision": "3960cacfe73f6887d665",
+    "url": "/StarWarsDB/static/js/1.3960cacf.chunk.js"
+  },
+  {
+    "revision": "c348d97c41185b2728d0",
+    "url": "/StarWarsDB/static/css/main.15877077.chunk.css"
+  },
+  {
+    "revision": "2228dcc0d9ee92d61448caded6a00ffc",
+    "url": "/StarWarsDB/index.html"
+  }
+];
