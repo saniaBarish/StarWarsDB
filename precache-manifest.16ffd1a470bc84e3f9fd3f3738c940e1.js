4f12381e974d2249cac6a34a28ee27f553cc134d
Barishpolets Oleksandr <barishpolets.oleksandr@pdffiller.team>
2018-12-11 18:44:13 +0200

    Updates

---
+++ precache-manifest.16ffd1a470bc84e3f9fd3f3738c940e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9d155c1563a8a449bea",
+    "url": "/StarWarsDB/static/js/runtime~main.f9d155c1.js"
+  },
+  {
+    "revision": "e5ce15b4c206e3beb86c",
+    "url": "/StarWarsDB/static/js/main.e5ce15b4.chunk.js"
+  },
+  {
+    "revision": "ea8dfc439f38bf8d9905",
+    "url": "/StarWarsDB/static/js/1.ea8dfc43.chunk.js"
+  },
+  {
+    "revision": "e5ce15b4c206e3beb86c",
+    "url": "/StarWarsDB/static/css/main.6cd31ee0.chunk.css"
+  },
+  {
+    "revision": "d22a39c15812fdaafb6f67ebd4bc0682",
+    "url": "/StarWarsDB/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/StarWarsDB/precache-manifest.3e7144ed0db31a6c6c845fe5e7c40f72.js"
+  "/StarWarsDB/precache-manifest.16ffd1a470bc84e3f9fd3f3738c940e1.js"
 );
 
 workbox.clientsClaim();
