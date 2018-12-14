da34927053a6193cf49154fe8a878e3e9b2b9c3d
Barishpolets Oleksandr <barishpolets.oleksandr@pdffiller.team>
2018-12-14 16:07:14 +0200

    add Redirect in App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
+import {BrowserRouter as Router, Route, Switch, Redirect} from "react-router-dom"
 
 import Header from "../Header";
 import RandomPlanet from "../RandomPlanet";
@@ -15,8 +15,10 @@ import "./App.css";
 export default class App extends Component {
 
   swapiService = new SwapiService();
+  homePage = "/StarWarsDB"
 
   render(){
+    console.log(this.homePage)
   
     return (
       <div>
@@ -25,26 +27,26 @@ export default class App extends Component {
           <Router>
             <div className="stardb-app">
             <ErrorBoundry>
-              <Header />
+              <Header homePage={`${this.homePage}`}/>
             </ErrorBoundry>
             <ErrorBoundry>
               <RandomPlanet />
               <Switch>
-                <Route path="/" 
+                <Route path={this.homePage} 
                   render={() => <h2>Welcome to StarWars DB</h2>}
                   exact
                 />
-                <Route path="/person/:id?" component={PersonPage} />
-                <Route path="/planet/:id?" component={PlanetPage} />
-                <Route path="/starship/" exact component={StarshipPage} />
-                <Route path="/starship/:id?" exact render={({match})=> {
+                <Route path={`${this.homePage}/person/:id?`} component={PersonPage} />
+                <Route path={`${this.homePage}/planet/:id?`} component={PlanetPage} />
+                <Route path={`${this.homePage}/starship/`} exact component={StarshipPage} />
+                <Route path={`${this.homePage}/starship/:id?`} exact render={({match})=> {
                   const {id} = match.params
-                  if (isNaN(+id / 1)){
+                  if (isNaN(id / 1)){
                     return <h2>Page not found:(</h2>
                   }
                   return <StarshipDetails id={id}/>
                 }} />
-                <Route render={() => <h2>Page not found:(</h2>}/>
+                <Redirect to={`${this.homePage}/`} />
               </Switch>
             </ErrorBoundry>
             </div>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,20 +3,20 @@ import { Link } from "react-router-dom"
 
 import "./Header.css";
 
-const Header = () => {
+const Header = ({homePage}) => {
     return (
       <div className="header d-flex">
         <h3>
-          <Link to="/">StarWars DB</Link>
+          <Link to={`${homePage}/`}>StarWars DB</Link>
         </h3>
         <ul className="d-flex">
-          <Link to="/person/" >
+          <Link to={`${homePage}/person/`} >
             <li>Person</li>
           </Link>
-          <Link to="/planet/" >
+          <Link to={`${homePage}/planet/`} >
             <li>Planets</li>
           </Link>
-          <Link to="/starship/" >
+          <Link to={`${homePage}/starship/`} >
             <li>Starships</li>
           </Link>
         </ul>
